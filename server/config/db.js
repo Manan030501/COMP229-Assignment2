ed21d87ca337dde6f9fb638a930576c925e4acde
Manan030501 <72701544+Manan030501@users.noreply.github.com>
2020-10-26 01:06:24 -0400

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 module.exports =
 {
     //"URI": "mongodb://localhost/book_store"
-    "URI": ""
+    "URI": "mongodb+srv://manan:WZNU9pmHtWq9f5Rx@cluster0.ndl30.mongodb.net/contacts-list?retryWrites=true&w=majority"
 }
 
 
